7e7f5fca8aee3067b751b91d0117634a63999325
Twinklehingu <twinkle.hingu29@gmail.com>
2024-10-24 23:23:22 -0700

    added property binding and interpolation

--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -2,8 +2,37 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-first',
-  templateUrl: './first.component.html',
-  styleUrls: ['./first.component.css']
+  template: `
+  <h2> First Component </h2>
+  <div>
+    Name: {{"Welcome, "+name}}
+    
+  </div>
+  <div>
+    {{name.length}}
+  </div>
+  <div>
+    {{name.toLowerCase()}}
+  </div>
+  <div>
+    {{name.toUpperCase()}}
+  </div>
+  <div>
+  {{testFn()}}
+  </div>
+
+  <hr> 
+  <h3>Property Bindings</h3>
+  <div>
+    <input [id] ="myId" type="text" value="Hello World"/>
+    <input [disabled]="false" id ={{myId}} type="text" value="Hello World"/>
+
+  </div>
+
+  {{2+4}}
+  
+  `,
+  styleUrls: []
 })
 export class FirstComponent implements OnInit {
 
@@ -12,4 +41,13 @@ export class FirstComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  public name = "John Smith!";
+  public myId = "testId";
+
+  testFn(){
+    return "String from Function"
+  }
 }
+
+//{{2+4}}
+  /////we can not assign the value like i = 2 + 4
