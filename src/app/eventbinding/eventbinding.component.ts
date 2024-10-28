62e48c3d96556b584a542e919207b3b9ecff126f
Twinklehingu <twinkle.hingu29@gmail.com>
2024-10-27 19:12:32 -0700

    added Log(TRV)

--- src/app/eventbinding/eventbinding.component.ts
+++ src/app/eventbinding/eventbinding.component.ts
@@ -5,8 +5,13 @@ import { Component, OnInit } from '@angular/core';
   template: `
     
     <h3>Eventbinding works!</h3>
-    <button (click)="onClick()">Click Me</button>
+    <button (click)="onClick($event)">Click Me</button>
+    <button (click)="message='Good Morning'">Hello</button>
     {{message}}
+    <hr>
+    <input type="text" #myInput>
+    <br>
+    <button (click)="LogMessage(myInput.value)">Log</button>
     
     
   `,
@@ -21,10 +26,13 @@ export class EventbindingComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  onClick(){
-    console.log("Button clicked")
-    this.message = "The button is clicked!"
+  onClick(event){
+    console.log(event)
+    // this.message = "The button is clicked!"
+    this.message = "Event type: " +event.type
   }
-
+  LogMessage(myInput){
+    console.log(myInput);
+  }
 
 }
